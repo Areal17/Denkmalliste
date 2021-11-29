476d01d42d66a903c1b28c8180b682685e28c5df
Areal17 <post@media-engineering.com>
2021-11-29 17:25:50 +0100

    Initial Commit

---
+++ Denkmalliste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Denkmalliste
+//
+//  Created by Ingo Wiederoder on 29.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Denkmalliste/DenkmallisteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenkmallisteApp.swift
+//  Denkmalliste
+//
+//  Created by Ingo Wiederoder on 29.11.21.
+//
+
+import SwiftUI
+
+@main
+struct DenkmallisteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
